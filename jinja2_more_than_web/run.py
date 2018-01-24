930bc52ca21f19fe41729ccbde7bd834da448a46
Chad Dotson <chad@cdotson.com>
2018-01-24 00:28:11 -0600

    initial commit

---
+++ jinja2_more_than_web/run.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from os.path import dirname, join
+from jinja2 import Environment, FileSystemLoader
+
+
+templates_path = join(dirname(__file__), "templates")
+templating_environment = Environment(loader=FileSystemLoader(templates_path),
+                                     lstrip_blocks=True,
+                                     trim_blocks=True)
+
+template = templating_environment.get_template("pirates.jinja2")
+
+pirates_and_ships = [
+    dict(captain="Blackbeard", ship="Queen Anne's Revenge"),
+    dict(captain="William Kidd", ship="Adventure Galley")
+]
+
+print(template.render(pirates=pirates_and_ships))
+
